c9221591c1f9f8054aeb0c8680ab7eb8a71a44bd
Leila Peltier <l_peltier@outlook.com>
2024-02-07 15:42:15 +0000

    Wrote tests for my getters

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,6 +1,53 @@
-public class BankAccountTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BankAccountTest{
+    BankAccount bankAccount;
+
+    // This method will be used to stramline the 'arrange' step in the testing methods
+    // now means that you won't have to keep setting up a new object each time
+
+    @BeforeEach
+    public void setUp(){
+        bankAccount = new BankAccount("Leila","Peltier","23/07/1999",12967);
+    }
+
+    @Test
+
+    public void canGetFirstName(){
+        String result = bankAccount.getFirstName();
+        assertThat(result).isEqualTo("Leila");
+    }
+
+    @Test
+    public void canGetLastName(){
+        String result = bankAccount.getLastName();
+        assertThat(result).isEqualTo("Peltier");
+    }
+
+    @Test
+
+    public void canGetDateOfBirth(){
+        String result = bankAccount.getDateOfBirth();
+        assertThat(result).isEqualTo("23/07/1999");
+    }
+
+    @Test
+
+    public void canGetAccountNumber(){
+        int result = bankAccount.getAccountNumber();
+        assertThat(result).isEqualTo(12967);
+    }
+
+    @Test
+
+    public void canGetBalance(){
+        int result = bankAccount.getBalance();
+        assertThat(result).isEqualTo(0);
+    }
 
 
 
 }
-
