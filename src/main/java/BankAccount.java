6f1a5dd2df9c62e357c40709d87caf407b81a70c
Leila Peltier <l_peltier@outlook.com>
2024-02-07 14:34:43 +0000

    added getters and setters to BankAccount class

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,18 +1,59 @@
 public class BankAccount {
 
+// Instance variables
     private String firstName ;
     private String lastName;
     private String dateOfBirth;
     private int accountNumber;
     private int balance;
 
+// Constructor
     public BankAccount (String inputFirstName, String inputLastName, String inputDateOfBirth, int inputAccountNumber){
     this.firstName = inputFirstName;
     this.lastName = inputLastName;
     this.dateOfBirth = inputDateOfBirth;
     this.accountNumber = inputAccountNumber;
     this.balance = 0;
-
     }
 
+// Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }
