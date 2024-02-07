31d1b4c77278b4b9cef9e96ad43f08975c0000b8
Leila Peltier <l_peltier@outlook.com>
2024-02-07 14:24:09 +0000

    Created BankAccount class

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,9 @@
 public class BankAccount {
+
+    private String firstName ;
+    private String lastName;
+    private String dateOfBirth;
+    private int accountNumber;
+    private double balance;
+
 }
